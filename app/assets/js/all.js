dd89d2138c39b5090ca339540080d0c19e3c69e1
paytonwu90 <sw9008k@gmail.com>
2023-01-28 05:55:41 +0800

    完成全部頁面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,22 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
