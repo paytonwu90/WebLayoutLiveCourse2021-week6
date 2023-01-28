79998ace66ebe4478327bc89a5e60cbec5b84760
paytonwu90 <sw9008k@gmail.com>
2023-01-28 18:16:48 +0800

    Update 2023-01-28T10:16:22.358Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,25 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
