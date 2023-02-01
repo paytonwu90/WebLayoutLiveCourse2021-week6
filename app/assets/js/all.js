fb7a12f1a3d51584ad001f2724528b7c35f5cad4
paytonwu90 <sw9008k@gmail.com>
2023-02-01 18:34:35 +0800

    增加 AOS 套件

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -20,3 +20,8 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+AOS.init({
+  duration: 800, // values from 0 to 3000, with step 50ms
+  once: true, // whether animation should happen only once - while scrolling down
+});
