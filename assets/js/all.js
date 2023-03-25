ad031620bcd7b1cd05d82ae91b81dccc283d23fe
paytonwu90 <sw9008k@gmail.com>
2023-03-25 21:34:34 +0800

    Update 2023-03-25T13:34:13.444Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -22,4 +22,10 @@ var swiper = new Swiper('.swiper', {
     }
   }
 });
+AOS.init({
+  duration: 800,
+  // values from 0 to 3000, with step 50ms
+  once: true // whether animation should happen only once - while scrolling down
+
+});
 //# sourceMappingURL=all.js.map
